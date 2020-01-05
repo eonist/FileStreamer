18b43c3e7c09298d8e3e9774abcec29f46694076
Eonist <30n1st@gmail.com>
2020-01-05 19:23:50 +0100

    Add asset based unit test

--- Tests/FileStreamerTests/FileStreamerTests.swift
+++ Tests/FileStreamerTests/FileStreamerTests.swift
@@ -1,5 +1,6 @@
-@testable import FileStreamer
 import XCTest
+import ResourceHelper
+import FileStreamer
 
 class FileStreamerTests: XCTestCase {
    override func setUp() {
@@ -9,12 +10,24 @@ class FileStreamerTests: XCTestCase {
       super.tearDown()
    }
    func testExample() {
-      XCTAssertEqual("Hello, World!", "Hello, World!")
-//      guard let fileSize: UInt64 = try? FileStreamReader.fileSize(filePath: fromFilePath) else { Swift.print("err - filesize"); return nil }
-//      guard let data: Data = try? FileStreamReader.read(filePath: fromFilePath, startIndex: 0, endIndex: Int(fileSize)) else { Swift.print("unable to read data"); return nil }
-//      guard !data.isEmpty else { Swift.print("err, missing file"); return nil }
+      XCTAssertTrue(readFile())
    }
    func testPerformanceExample() {
       self.measure { }
+   }
+}
+extension FileStreamerTests {
+   /**
+    * Test Filesize and
+    */
+   func readFile() -> Bool {
+      let fromFilePath: String = ResourceHelper.projectRootURL(projectRef: #file, fileName: "temp.bundle/demo.mp3").path
+      guard let fileSize: UInt64 = try? FileStreamReader.fileSize(filePath: fromFilePath) else { Swift.print("err - filesize"); return false }
+      Swift.print("fileSize:  \(fileSize)")
+      guard let data: Data = try? FileStreamReader.read(filePath: fromFilePath, startIndex: 0, endIndex: Int(fileSize)) else { Swift.print("unable to read data"); return false }
+      guard !data.isEmpty else { Swift.print("err, missing file"); return false }
+      Swift.print("data.count:  \(data.count)")
+      Swift.print("data is readable ✅")
+      return true
    }
 }
